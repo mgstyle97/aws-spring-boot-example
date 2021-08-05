135aeef34d78eb4138fbc6f4e5829b3d54b93fbe
Mingi Kim <migni4575@gmail.com>
2021-08-05 21:26:38 +0900

    Modified Test Code(remove unused import) Commit

--- src/test/java/com/example/web/HelloControllerTest.java
+++ src/test/java/com/example/web/HelloControllerTest.java
@@ -3,12 +3,9 @@ package com.example.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = HelloController.class)
