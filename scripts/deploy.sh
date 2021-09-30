e66b20fb557ca34d937b73ed09fae4a632445297
Mingi Kim <migni4575@gmail.com>
2021-09-30 15:07:20 +0900

    Modified deploy.sh to execute java -jar script issue#1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,12 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=migni-springboot-webservice
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 # 실행 중이면 종료하기 위해서 현재 수행 중인 프로세스id를 찾습니다.
-# springboot2-webservice으로 된 다른 프로그램들이 있을 수 있어 springboot2-webservice된 jar 프로세스를 찾은 뒤 id를 찾습니다(awk '{print $1}').
-CURRENT_PID=$(pgrep -fl migni-springboot-webservice | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl migni-springboot-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
